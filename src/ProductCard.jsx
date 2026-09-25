dcb1071b9d0f3bf53ff3dbd39354b69aef7590b5
ea2045627ai <ea2045627@gmail.com>
2026-09-25 17:14:51 +0000

    commit

--- src/ProductCard.jsx
+++ src/ProductCard.jsx
@@ -180,6 +180,7 @@ export default function ProductCatalog({
                     >
                       Accept Route
                     </button>
+                  </div>
                 </div>
               </div>
             ))}
